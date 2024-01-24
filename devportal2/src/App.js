16f6a85b8c4f9357b1b3785cc43fade9cc02f556
sadan <muthunayakahst.21@uom.lk>
2024-01-24 18:33:06 +0530

    third commit

---
+++ devportal2/src/App.js
@@ -0,0 +1,27 @@
+
+import { Box } from '@mui/material';
+//import { Header, Header2 } from './Header';
+// import Header, { Header2 } from './Header';
+
+
+import Header from './Header';
+// import Header2  from './Header';
+import VerticalNav from './VerticalNav';
+// import Area from './Area';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Box>
+      <Header/>
+       {/* <Area/>  */}
+      <VerticalNav/>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
